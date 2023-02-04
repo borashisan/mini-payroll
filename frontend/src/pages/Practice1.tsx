a330421ac752490a96754bea8443dcabd75f8deb
borashisan <tomochuro731@gmail.com>
2023-02-05 00:30:07 +0900

    react-final-form

--- frontend/src/pages/Practice1.tsx
+++ frontend/src/pages/Practice1.tsx
@@ -1,10 +1,59 @@
 import { type FC } from 'react'
-// import { Link } from 'react-router-dom'
+import { Form, Field } from 'react-final-form'
+
+const sleep = async (ms: number): Promise<void> => {
+  await new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const onSubmit = async (values: object): Promise<void> => {
+  await sleep(300)
+  window.alert(JSON.stringify(values))
+}
 
 const Practice1: FC = () => {
   return (
     <>
       <h1>問題1</h1>
+      <div>
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>First Name</label>
+                <Field
+                  name="firstName"
+                  component="input"
+                  type="text"
+                  placeholder="First Name"
+                />
+              </div>
+              <div>
+                <label>Last Name</label>
+                <Field
+                  name="lastName"
+                  component="input"
+                  type="text"
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="buttons">
+                <button type="submit" disabled={submitting || pristine}>
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  onClick={form.reset}
+                  disabled={submitting || pristine}
+                >
+                  Reset
+                </button>
+              </div>
+              <pre>{JSON.stringify(values)}</pre>
+            </form>
+          )}
+        />
+      </div>
     </>
   )
 }
