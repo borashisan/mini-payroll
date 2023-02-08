e0bbfaf8db0e37043a90c2e375692cfbebac15e9
borashisan <tomochuro731@gmail.com>
2023-02-09 08:25:34 +0900

    rubocop -A

--- backend/app/controllers/practices_controller.rb
+++ backend/app/controllers/practices_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class PracticesController < ApplicationController
   def practice1
     basis_for_extra_pay = PayDeductionService.new.calculate_basis_for_extra_pay(params)
-    render json: { payload: { basis_for_extra_pay: basis_for_extra_pay } }
+    render json: { payload: { basis_for_extra_pay: } }
   end
 end
