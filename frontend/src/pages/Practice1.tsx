1048289278812727a481d15d31b5018dd44bacf6
borashisan <tomochuro731@gmail.com>
2023-02-11 21:04:46 +0900

    initialValuesを切り替えられるように修正

--- frontend/src/pages/Practice1.tsx
+++ frontend/src/pages/Practice1.tsx
@@ -37,6 +37,50 @@ type AllowancesType = {
   otherAllowance?: OtherAllowanceValuesType[]
 }
 
+const case1: AllowancesType = {
+  baseSalary: { value: '320000', isRelatedLabor: true },
+  positionAllowance: { value: '70000', isRelatedLabor: true },
+  housingAllowance: {
+    value: '35000',
+    isRelatedLabor: false,
+    isUniform: false,
+  },
+  commutingAllowance: {
+    value: '14000',
+    isRelatedLabor: false,
+    isUniform: false,
+    payUnit: '1month',
+  },
+}
+
+const case2: AllowancesType = {
+  baseSalary: { value: '215000', isRelatedLabor: true },
+  qualificationAllowance: { value: '5000', isRelatedLabor: true },
+  housingAllowance: {
+    value: '10000',
+    isRelatedLabor: false,
+    isUniform: true,
+  },
+  commutingAllowance: {
+    value: '24372',
+    isRelatedLabor: false,
+    isUniform: false,
+    payUnit: '3month',
+  },
+  otherAllowance: [
+    {
+      name: '家族手当',
+      value: '16000',
+      isRelatedLabor: false,
+      isUniform: false,
+    },
+  ],
+}
+
+const freeFormat: AllowancesType = {
+  baseSalary: { value: '0', isRelatedLabor: true },
+}
+
 const required = (value: inputValue): ErrorMessage =>
   value !== undefined ? undefined : '値を入力してください'
 
@@ -55,49 +99,17 @@ const composeValidators =
     )
 
 const Practice1: FC = () => {
-  //  const initialValues = {
-  //    baseSalary: { value: '320000', isRelatedLabor: true },
-  //    positionAllowance: { value: '70000', isRelatedLabor: true },
-  //    housingAllowance: {
-  //      value: '35000',
-  //      isRelatedLabor: false,
-  //      isUniform: false,
-  //    },
-  //    commutingAllowance: {
-  //      value: '14000',
-  //      isRelatedLabor: false,
-  //      isUniform: false,
-  //      payUnit: '1month',
-  //    },
-  //  }
-
-  const initialValues: AllowancesType = {
-    baseSalary: { value: '215000', isRelatedLabor: true },
-    qualificationAllowance: { value: '5000', isRelatedLabor: true },
-    housingAllowance: {
-      value: '10000',
-      isRelatedLabor: false,
-      isUniform: true,
-    },
-    commutingAllowance: {
-      value: '24372',
-      isRelatedLabor: false,
-      isUniform: false,
-      payUnit: '3month',
-    },
-    otherAllowance: [
-      {
-        name: '家族手当',
-        value: '16000',
-        isRelatedLabor: false,
-        isUniform: false,
-      },
-    ],
-  }
-
+  const [initialValues, setInitializeValues] = useState(case1)
   return (
     <>
       <h1>問題1</h1>
+      <div>
+        <button onClick={() => setInitializeValues(case1)}>事例1</button>
+        <button onClick={() => setInitializeValues(case2)}>事例2</button>
+        <button onClick={() => setInitializeValues(freeFormat)}>
+          自由記述
+        </button>
+      </div>
       <div>
         <AllowanceForm initialValues={initialValues} />
       </div>
