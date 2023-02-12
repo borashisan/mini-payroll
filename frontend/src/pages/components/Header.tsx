a2e82e968d8169015ace9f3980614d777d769247
borashisan <tomochuro731@gmail.com>
2023-02-12 10:10:28 +0900

    Headerにリンク追加

--- frontend/src/pages/components/Header.tsx
+++ frontend/src/pages/components/Header.tsx
@@ -1,9 +1,12 @@
 import { type FC } from 'react'
+import { Link } from 'react-router-dom'
 
 const Header: FC = () => {
   return (
     <div className="p-4 border border-gray-100">
-      <h1 className="text-3xl font-semibold">ミニ給与</h1>
+      <h1 className="text-3xl font-semibold">
+        <Link to={'/'}>ミニ給与</Link>
+      </h1>
     </div>
   )
 }
